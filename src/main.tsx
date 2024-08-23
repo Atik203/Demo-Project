305d52cf326b205cea8d31f3d8eaa20214e20133
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-08-23 13:57:11 +0600

    added syncfusion spreadsheet

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
-import "@fortune-sheet/react/dist/index.css";
+import { registerLicense } from "@syncfusion/ej2-base";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
 import router from "./routes/routes.tsx";
+
+registerLicense(
+  "Ngo9BigBOggjHTQxAR8/V1NCaF1cWWhBYVB2WmFZfVpgdVdMZVVbQH5PMyBoS35RckVrW3xecHFcQmhcWEV2"
+);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,21 @@
 import React from "react";
-
-import "@fortune-sheet/react/dist/index.css";
-import DashboardMenu from "../components/ui/DashboardMenu";
-import SpreadSheet from "../components/ui/SpreadSheet";
+import MainSheet from "../components/ui/MainSheet";
 
 const Home: React.FC = () => {
   return (
-    <div className="grid grid-cols-12 min-h-screen">
-      <DashboardMenu />
-      <main className="col-span-10 bg-gray-100 p-8">
-        <header className="mb-8">
-          <h1 className="text-3xl font-bold">Dashboard</h1>
-        </header>
-        <section className="mb-8">
-          <SpreadSheet />
-        </section>
-      </main>
+    // <div className="grid grid-cols-12 min-h-screen">
+    //   <DashboardMenu />
+    //   <main className="col-span-10 bg-gray-100 p-8">
+    //     <header className="mb-8">
+    //       <h1 className="text-3xl font-bold">Dashboard</h1>
+    //     </header>
+    //     <section className="mb-8">
+    //       <MainSheet />
+    //     </section>
+    //   </main>
+    // </div>
+    <div>
+      <MainSheet />
     </div>
   );
 };
